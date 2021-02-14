c55622af00873010a9f13bd1ff2fabe75421bbe9
DG <gchakradhar72@gmail.com>
2021-02-14 18:07:14 +0530

    Updated logic so that a blank link doesn't add the href in the title

--- render.js
+++ render.js
@@ -1,7 +1,7 @@
 const resultsDiv = document.getElementById('results');
 
 
-function createCustomElement(elemType, clsName, inhtml) {
+function newElem(elemType, clsName, inhtml) {
 	let element = document.createElement(elemType);
 	element.className = clsName;
 	if (inhtml) element.innerHTML = inhtml;
@@ -16,11 +16,12 @@ function searchQ() {
 	if (inp != '') {
 		for (let d of data) {
 			if (d.name.toLowerCase().includes(inp.toLowerCase())) {
-				let card = createCustomElement('div', 'card');
+				let card = newElem('div', 'card');
 				card.style.maxHeight = '59px';
 				resultsDiv.appendChild(card);
 
-				let title = createCustomElement('div', 'title-wrapper', `<a href=${d.link} target="_blank">${d.name}</a>`);
+				let link = (d.link.length === 0) ? ` ` : ` href=${d.link} `;
+				let title = newElem('div', 'title-wrapper', `<a${link}target="_blank">${d.name}</a>`);
 				title.onclick = () => {
 					let tempCard = title.parentElement;
 					tempCard.style.maxHeight = (tempCard.style.maxHeight == '59px') ? (tempCard.scrollHeight + 'px') : '59px';
@@ -28,20 +29,20 @@ function searchQ() {
 				card.appendChild(title);
 				
 				if (d.cont.length != 0)
-					card.appendChild(createCustomElement('div', 'cont', d.cont));
+					card.appendChild(newElem('div', 'cont', d.cont));
 
 				if (d.tags.length != 0) {
-					let tagWrapper = createCustomElement('div', 'wrapper');
+					let tagWrapper = newElem('div', 'wrapper');
 					card.appendChild(tagWrapper);
 					for (let t of d.tags)
-						tagWrapper.appendChild(createCustomElement('a', 'tag1', t));
+						tagWrapper.appendChild(newElem('a', 'tag1', t));
 				}
 				
 				if (d.proj.length != 0) {
-					let projWrapper = createCustomElement('div', 'wrapper');
+					let projWrapper = newElem('div', 'wrapper');
 					card.appendChild(projWrapper);
 					for (let p of d.proj)
-						projWrapper.appendChild(createCustomElement('a', 'tag2', p));
+						projWrapper.appendChild(newElem('a', 'tag2', p));
 						// tag.href = 'https://google.com';
 				}
 			}
